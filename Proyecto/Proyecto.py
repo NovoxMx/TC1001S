f3bc075dba39c43a9f0adf37358802f664fada85
NovoxMx <89361425+NovoxMx@users.noreply.github.com>
2022-05-13 19:28:53 -0700

    Se añadio llave, 2 mapas más, mapa random, y comentarios

--- Proyecto/Proyecto.py
+++ Proyecto/Proyecto.py
@@ -1,10 +1,26 @@
+# Equipo 4
+# Integrantes:
+# A01254180 - André Castillo Machado
+# A01254155 - Marco Tulio Montoya Angulo
+# A00835132 - Ángel de Fidel Marín González
+# A01254831 - Luis Carlos Rico Almada
+
+# Proyecto TC1001S - Laberinto
+
 from turtle import *
 from freegames import floor, vector
+import random
 
+# Se definen variables
 comparador = True
+llave = False
 path = Turtle(visible=False)
 
-mapa = [
+# Creación de mapas
+# Hecho por: Marco Montoya
+mapa = []
+
+mapa1 = [
     0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
     0, 1, 1, 1, 0, 1, 0, 1, 1, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 1, 1, 1, 1, 0,
     0, 1, 0, 1, 0, 1, 0, 0, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 0, 0, 1, 0, 1, 0,
@@ -14,7 +30,7 @@ mapa = [
     0, 1, 0, 1, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 1, 0, 1, 0, 1, 0, 0, 0, 1, 0,
     0, 1, 1, 1, 0, 1, 0, 1, 1, 1, 0, 1, 1, 1, 0, 1, 0, 1, 0, 1, 1, 1, 0, 1, 0,
     0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0,
-    0, 1, 1, 1, 0, 1, 0, 1, 1, 1, 0, 1, 1, 1, 0, 1, 0, 1, 0, 1, 1, 1, 1, 1, 0,
+    0, 1, 1, 1, 0, 1, 0, 1, 1, 1, 0, 1, 1, 4, 0, 1, 0, 1, 0, 1, 1, 1, 1, 1, 0,
     0, 0, 0, 1, 0, 1, 0, 0, 0, 1, 0, 1, 0, 0, 0, 1, 0, 1, 0, 0, 0, 1, 0, 1, 0,
     0, 1, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 1, 0, 1, 0, 1, 0, 1, 1, 1, 0, 1, 0,
     0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 1, 0, 1, 0, 0, 0, 0, 0, 1, 0,
@@ -28,11 +44,49 @@ mapa = [
     0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0,
     0, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 1, 0, 1, 0, 1, 0, 1, 0,
     0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 1, 0, 1, 0, 1, 0, 0, 0, 1, 0, 1, 0, 1, 0,
-    0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 3, 1, 1, 1, 0, 1, 1, 1, 0,
+    0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 0,
     0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0,
 ]
 
+mapa2 = [
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 0,
+    0, 1, 0, 1, 1, 1, 0, 1, 1, 1, 0, 1, 0, 1, 1, 1, 0, 1, 0, 1, 1, 1, 0, 1, 0,
+    0, 1, 0, 1, 0, 1, 0, 0, 0, 1, 0, 1, 0, 0, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0,
+    0, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 0, 1, 0, 1, 0, 1, 1, 1, 0,
+    0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 1, 0, 1, 0, 0, 0, 1, 0,
+    0, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 0,
+    0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0,
+    0, 1, 1, 1, 0, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 4, 0, 1, 0, 1, 0, 1, 1, 1, 0,
+    0, 1, 0, 1, 0, 1, 0, 0, 0, 1, 0, 1, 0, 1, 0, 0, 0, 1, 0, 1, 0, 1, 0, 1, 0,
+    0, 1, 0, 1, 1, 1, 1, 1, 1, 1, 0, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 1, 0, 1, 0,
+    0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0,
+    0, 1, 1, 1, 1, 1, 0, 1, 1, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 1, 1, 1, 1, 0,
+    0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0,
+    0, 1, 1, 1, 0, 1, 0, 1, 1, 1, 0, 1, 0, 1, 1, 1, 1, 1, 1, 1, 0, 1, 0, 1, 0,
+    0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 1, 0, 1, 0, 1, 1, 1, 1, 1, 0, 1, 0, 1, 1, 1, 0, 1, 1, 1, 0, 1, 0, 1, 0,
+    0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0,
+    0, 1, 1, 1, 0, 1, 0, 1, 0, 1, 0, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 0, 1, 0,
+    0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 1, 0, 0, 0, 1, 0, 1, 0, 1, 0, 0, 0, 1, 0,
+    0, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 1, 0, 1, 0, 1, 1, 1, 1, 1, 0,
+    0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0,
+    0, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 0,
+    0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0,
+    0, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0,
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0,
+]
 
+
+# Se selecciona de manera aleatoria el mapa
+# Hecho por: Luis Rico
+n_random = random.randint(0,1)
+if n_random == 0:
+    mapa = mapa1
+elif n_random == 1:
+    mapa = mapa2
+
+# Funcion para dibujar un cuadrado en el mundo
+# Hecho por: Angel Marin
 def square(x, y):
     "Draw square using path at (x, y)."
     path.up()
@@ -40,22 +94,25 @@ def square(x, y):
     path.down()
     path.begin_fill()
 
-    for count in range(4):
+    for count in range(4):  # Se dibuja el cuadro por partes
         path.forward(24)
         path.left(90)
 
     path.end_fill()
 
+
+# Se crea el "mundo" usando las funciones de turtle
+# Hecho por: Ángel Marín
 def world():
     "Draw world using path."
-    bgcolor('green')
+    bgcolor('chartreuse3')
 
     for index in range(len(mapa)):
         tile = mapa[index]
 
         if tile == 1:
-            path.color('grey')
-            x = (index % 25) * 25 - 310
+            path.color('bisque')    # Se define el color del cuadro
+            x = (index % 25) * 25 - 310     # Coordenadas del cuadro
             y = 290 - (index // 25) * 25
             square(x, y)
         elif tile == 2:
@@ -64,12 +121,18 @@ def world():
             y = 290 - (index // 25) * 25
             square(x, y)
         elif tile == 3:
-            path.color('yellow')
+            path.color('saddlebrown')
+            x = (index % 25) * 25 - 310
+            y = 290 - (index // 25) * 25
+            square(x, y)
+        elif tile == 4:
+            path.color('gold')
             x = (index % 25) * 25 - 310
             y = 290 - (index // 25) * 25
             square(x, y)
 
-
+# Se busca la posicion actual del personaje
+# Hecho por: Marco Montoya
 def encontrar(lista):
     for v in range(625):
         if lista[v] == 2:
@@ -77,53 +140,72 @@ def encontrar(lista):
         else: 
             continue
         
+# Funciones para mover al personaje dentro del laberinto
+# Hecho por: André Castilo
 
+# Ejemplo base:
 def arriba():
-    posicion = encontrar(mapa)
+    posicion = encontrar(mapa)  # Se obtiene la posicion del personaje
+    global llave
     if (mapa[posicion-25] == 1):
+        mapa[posicion-25] = 2   
+        mapa[posicion] = 1      # Se modifica la matriz
+    elif (mapa[posicion-25] == 4):
         mapa[posicion-25] = 2
         mapa[posicion] = 1
-    elif (mapa[posicion-25] == 3):
+        llave = True
+    elif (mapa[posicion-25] == 3 and llave == True):
         mapa[posicion-25] = 2
         mapa[posicion] = 1
         tracer(False)
-        world() 
-        done()
- 
+        world()         
+        done()  # Se acaba el juego
 
 def izquierda():
     posicion = encontrar(mapa)
+    global llave
     if (mapa[posicion-1] == 1):
         mapa[posicion-1] = 2
         mapa[posicion] = 1
-    elif (mapa[posicion-1] == 3):
+    elif (mapa[posicion-1] == 4):
+        mapa[posicion-1] = 2
+        mapa[posicion] = 1
+        llave = True
+    elif (mapa[posicion-1] == 3 and llave == True):
         mapa[posicion-1] = 2
         mapa[posicion] = 1
         tracer(False)
         world() 
         done()
-  
-
 
 def abajo():
     posicion = encontrar(mapa)
+    global llave
     if (mapa[posicion+25] == 1):
         mapa[posicion+25] = 2
         mapa[posicion] = 1
-    elif (mapa[posicion+25] == 3):
+    elif (mapa[posicion+25] == 4):
+        mapa[posicion+25] = 2
+        mapa[posicion] = 1
+        llave = True
+    elif (mapa[posicion+25] == 3 and llave == True):
         mapa[posicion+25] = 2
         mapa[posicion] = 1
         tracer(False)
         world() 
         done()
 
-
 def derecha():
     posicion = encontrar(mapa)
+    global llave
     if (mapa[posicion+1] == 1):
         mapa[posicion+1] = 2
         mapa[posicion] = 1
-    elif (mapa[posicion+1] == 3):
+    elif (mapa[posicion-1] == 4):
+        mapa[posicion-1] = 2
+        mapa[posicion] = 1
+        llave = True
+    elif (mapa[posicion+1] == 3 and llave == True):
         mapa[posicion+1] = 2
         mapa[posicion] = 1
         tracer(False)
@@ -131,13 +213,19 @@ def derecha():
         done()
 
 
+
+# Se genera la ventana del turtle
+# Hecho por: Marco Montoya
 setup(700, 700)
 hideturtle()
+
+# Ciclo while que repite el ciclo cada que se presiona una tecla
+# Hecho por: André Castillo
 while comparador == True:
     tracer(False)
-    world()  
+    world()     # Se dibuja el mundo
     listen() 
-    onkeypress(arriba, 'w')
+    onkeypress(arriba, 'w')     # Detección de teclas oprimidas
     onkeypress(abajo, 's')
     onkeypress(derecha, 'd')
     onkeypress(izquierda, 'a')
