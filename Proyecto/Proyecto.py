d179cfbd91d98d919710924a9a104cdc9b6db34e
NovoxMx <89361425+NovoxMx@users.noreply.github.com>
2022-05-13 19:26:13 -0700

    Se agrego movimiento, y donde encontrar al personaje -André y mapa -Marco

--- Proyecto/Proyecto.py
+++ Proyecto/Proyecto.py
@@ -1,73 +1,143 @@
-from random import choice
 from turtle import *
 from freegames import floor, vector
-from time import *
 
+comparador = True
 path = Turtle(visible=False)
-        
-niveles = []
 
-tiles = [
-    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-    1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-    2, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
+mapa = [
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 1, 1, 1, 0, 1, 0, 1, 1, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 1, 1, 1, 1, 0,
+    0, 1, 0, 1, 0, 1, 0, 0, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 0, 0, 1, 0, 1, 0,
+    0, 1, 0, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 0, 1, 0, 1, 1, 1, 1, 1, 0, 1, 0,
+    0, 0, 0, 1, 0, 1, 0, 1, 0, 0, 0, 0, 0, 1, 0, 1, 0, 1, 0, 0, 0, 1, 0, 1, 0,
+    0, 1, 0, 1, 0, 1, 0, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 0, 1, 0,
+    0, 1, 0, 1, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 1, 0, 1, 0, 1, 0, 0, 0, 1, 0,
+    0, 1, 1, 1, 0, 1, 0, 1, 1, 1, 0, 1, 1, 1, 0, 1, 0, 1, 0, 1, 1, 1, 0, 1, 0,
+    0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0,
+    0, 1, 1, 1, 0, 1, 0, 1, 1, 1, 0, 1, 1, 1, 0, 1, 0, 1, 0, 1, 1, 1, 1, 1, 0,
+    0, 0, 0, 1, 0, 1, 0, 0, 0, 1, 0, 1, 0, 0, 0, 1, 0, 1, 0, 0, 0, 1, 0, 1, 0,
+    0, 1, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 1, 0, 1, 0, 1, 0, 1, 1, 1, 0, 1, 0,
+    0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 1, 0, 1, 0, 0, 0, 0, 0, 1, 0,
+    0, 1, 1, 1, 0, 1, 0, 1, 0, 1, 0, 1, 1, 1, 0, 1, 1, 1, 0, 1, 0, 1, 0, 1, 0,
+    0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 0, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0,
+    0, 1, 0, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 0, 1, 1, 1, 1, 1, 0,
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0,
+    0, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 0, 1, 0,
+    0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 1, 0,
+    0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0,
+    0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0,
+    0, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 1, 0, 1, 0, 1, 0, 1, 0,
+    0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 1, 0, 1, 0, 1, 0, 0, 0, 1, 0, 1, 0, 1, 0,
+    0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 3, 1, 1, 1, 0, 1, 1, 1, 0,
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0,
 ]
 
 
 def square(x, y):
-
+    "Draw square using path at (x, y)."
     path.up()
     path.goto(x, y)
     path.down()
     path.begin_fill()
 
     for count in range(4):
-        path.forward(20)
+        path.forward(24)
         path.left(90)
 
     path.end_fill()
 
-
-
 def world():
+    "Draw world using path."
+    bgcolor('green')
 
-    bgcolor('black')
-    path.color('blue')
+    for index in range(len(mapa)):
+        tile = mapa[index]
 
-    for index in range(len(tiles)):
-        tile = tiles[index]
-
-        if tile > 0:
-            x = (index % 20) * 20 - 200
-            y = 180 - (index // 20) * 20
+        if tile == 1:
+            path.color('grey')
+            x = (index % 25) * 25 - 310
+            y = 290 - (index // 25) * 25
+            square(x, y)
+        elif tile == 2:
+            path.color('red')
+            x = (index % 25) * 25 - 310
+            y = 290 - (index // 25) * 25
+            square(x, y)
+        elif tile == 3:
+            path.color('yellow')
+            x = (index % 25) * 25 - 310
+            y = 290 - (index // 25) * 25
             square(x, y)
 
-            if tile == 1:
-                path.up()
-                path.goto(x + 10, y + 10)
-                path.dot(2, 'white')
+
+def encontrar(lista):
+    for v in range(625):
+        if lista[v] == 2:
+            return v
+        else: 
+            continue
+        
+
+def arriba():
+    posicion = encontrar(mapa)
+    if (mapa[posicion-25] == 1):
+        mapa[posicion-25] = 2
+        mapa[posicion] = 1
+    elif (mapa[posicion-25] == 3):
+        mapa[posicion-25] = 2
+        mapa[posicion] = 1
+        tracer(False)
+        world() 
+        done()
+ 
+
+def izquierda():
+    posicion = encontrar(mapa)
+    if (mapa[posicion-1] == 1):
+        mapa[posicion-1] = 2
+        mapa[posicion] = 1
+    elif (mapa[posicion-1] == 3):
+        mapa[posicion-1] = 2
+        mapa[posicion] = 1
+        tracer(False)
+        world() 
+        done()
+  
+
+
+def abajo():
+    posicion = encontrar(mapa)
+    if (mapa[posicion+25] == 1):
+        mapa[posicion+25] = 2
+        mapa[posicion] = 1
+    elif (mapa[posicion+25] == 3):
+        mapa[posicion+25] = 2
+        mapa[posicion] = 1
+        tracer(False)
+        world() 
+        done()
+
+
+def derecha():
+    posicion = encontrar(mapa)
+    if (mapa[posicion+1] == 1):
+        mapa[posicion+1] = 2
+        mapa[posicion] = 1
+    elif (mapa[posicion+1] == 3):
+        mapa[posicion+1] = 2
+        mapa[posicion] = 1
+        tracer(False)
+        world() 
+        done()
 
 
 setup(700, 700)
 hideturtle()
-tracer(False)       
-listen()
-world()
-done()
+while comparador == True:
+    tracer(False)
+    world()  
+    listen() 
+    onkeypress(arriba, 'w')
+    onkeypress(abajo, 's')
+    onkeypress(derecha, 'd')
+    onkeypress(izquierda, 'a')
